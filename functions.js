edb93866b53f259ce4c6bddcc02c6c55674f1c26
dallasmay <dmmay9@gmail.com>
2022-07-11 11:37:08 -0600

    completed functions

--- functions.js
+++ functions.js
@@ -28,4 +28,35 @@ const uniqueChar = (str) => {
 
 // EXPLANATION: I believe this would also be O(n^2), as it's a nested for loop. Same concept as my previous
 
+const isPangram = (str) => {
+    let lowerCase = str.toLowerCase();
+    let alpha = "abcdefghijklmnopqrstuvwxyz"
+    let alphaArr = alpha.split("")
+    for (let i = 0; i < alphaArr.length; i++) {
+        if (str.includes(alphaArr[i])) {
+            console.log(`String contains ${alphaArr[i]}`)
+        } else {
+            console.log(`String does not contain ${alphaArr[i]}`)
+            return false
+        }
+    }
+    return true;
+}
 
+// console.log(isPangram("The quick brow fox jumps over the lazy dog!"))
+
+// EXPLANATION: I believe this function would be also be O(n^2), as I am using .includes inside a for loop. Nested loop.
+
+// LONGEST WORD
+const findLongestWord = (arr) => {
+    let longestLength = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].length > longestLength) {
+            longestLength = arr[i].length;
+        }
+    }
+    return longestLength;
+}
+// console.log(findLongestWord(["hi", "hello", "yoyoyoyoyo"]))
+
+// EXPLANATION: Finally something different. I think this one is O(n), as I'm only ever looping one thing at a time.
